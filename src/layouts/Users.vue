32dfab0fb2ae7e3a8c22e15c6b9824ab0763e92d
Nigina Gafurova <nigina.gafurova.2001@mail.ru>
2023-03-06 16:02:45 +0300

    first commit

---
+++ src/layouts/Users.vue
@@ -0,0 +1,42 @@
+<template>
+    <!-- <q-layout>
+
+         
+    <div class="flex flex-center text-h2 text-weight-bold text-light-green-13 q-mb-md">
+        Meow
+           
+    </div>
+    
+    <q-page-container>
+        <router-view></router-view>
+    </q-page-container>
+
+    </q-layout> -->
+    <q-layout view="hHh lpR fFf">
+
+    <q-header class="bg-pink-10 text-white ">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Little meow-meow-header
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container class="flex items-center justify-center">
+      <router-view />
+    </q-page-container>
+
+    <q-footer reveal class="bg-pink-12 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          
+          <div>Little meow-meow-footer</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
---
+++ src/pages/Profile.vue
@@ -0,0 +1,12 @@
+<template>
+ 
+    <q-page>
+        <div class="flex column flex-center">
+            <img src="~assets/cat.jpg" alt="" class="q-mb-md"> 
+            <q-btn color="deep-orange" glossy label="Kitty" />
+        </div>
+    
+    </q-page>
+
+</template>
+
