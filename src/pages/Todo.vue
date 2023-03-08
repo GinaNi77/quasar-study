2967376022d81271448718e55dbbda433e862da4
Nigina Gafurova <nigina.gafurova.2001@mail.ru>
2023-03-08 12:50:53 +0300

    Установили uuid для автоматизированного присваивания id элементам при их добавлении в список

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,34 +1,50 @@
 <template>
 
   <q-page class="flex flex-center">
-   <q-card class="my-card bg-pink-1" style="width: 600px">
+   <q-card class="my-card no-shadow" style="width: 600px">
       <q-card-section>
        <div class="text-h4 text-weight-bol text-pink-10 text-center">My Todo List</div>
       </q-card-section>
 
-       <div class="row justify-center">
-            <div class="q-pb-md " v-if="editing">
-              <q-btn round dense flat icon="close" @click="doEdit(false)" color="pink-8"/>
+       <q-card-section class="row justify-center">
+            <div v-if="editing">
+              <q-btn round dense flat
+              icon="close"
+              @click="doEdit(false)"
+              color="pink-8"/>
             </div>
-            <div class="q-pb-md " v-else>
-              <q-btn round dense flat icon="edit" @click="doEdit(true)" color="pink-8"/>
+
+            <div v-else>
+              <q-btn round dense flat
+              icon="edit"
+              @click="doEdit(true)"
+              color="pink-8"/>
             </div>
-        </div>
+        </q-card-section>
 
       <q-card-section v-if:="editing">
         <div class="row justify-center">
           <div class="col-10">
            <q-input outlined v-model="newTodo" label="add todo">
+
             <template v-slot:after>
-              <q-btn round dense flat icon="send" class="text-pink-10" @click="addTodo" :disabled="newTodo===''"/>
+              <q-btn round dense flat
+              icon="send"
+              class="text-pink-10"
+              @click="addTodo"
+              :disabled="newTodo===''"/>
             </template>
+
            </q-input>
           </div>
         </div>
       </q-card-section>
 
       <q-card-section v-if="todoList.length>0">
-        <div class="row justify-center" v-for="todo in todoList" :key="todo.id" @click="todo.done=!todo.done">
+        <div class="row justify-center"
+          v-for="todo in todoList"
+          :key="todo.id"
+          @click="todo.done=!todo.done">
           <div class="col-auto q-mr-sm q-pb-md">
             <q-checkbox v-model="todo.done" color="pink-10"/>
           </div>
@@ -42,7 +58,7 @@
       </q-card-section>
 
       <q-card-section v-else>
-        <div class="row justify-center text-h6 ">
+        <div class="row justify-center text-h6 text-pink-10">
           No todos
         </div>
       </q-card-section>
@@ -54,6 +70,7 @@
 
 <script>
 import {defineComponent} from 'vue'
+import { v4 as uuidv4 } from 'uuid';
 
 export default defineComponent({
   name: 'IndexPage',
@@ -61,16 +78,12 @@ export default defineComponent({
     return{
       editing: false,
       newTodo:"",
-      todoList: [
-        {id:"1", value: "buy apples", done: false},
-        {id:"2", value: "buy bananas", done: false},
-        {id:"3", value: "buy cherry", done: false}
-      ]    
+      todoList: []    
       }
   },
   methods: {
     addTodo(){
-      this.todoList.push({id: this.todoList.length+1, value: this.newTodo, done:false })
+      this.todoList.push({id: uuidv4(), value: this.newTodo, done:false })
       this.newTodo=""
     },
     deleteTodo(index){
